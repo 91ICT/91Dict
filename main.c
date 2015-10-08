926439c2615c4ee5b81ae6fdd260a8581e5c3595
Sam Hoang <samhv.ict@gmail.com>
2015-10-09 00:17:22 +0700

    Add: init btree, get new widget from builder

--- main.c
+++ main.c
@@ -1,54 +1,73 @@
 #include "support.h"
+#include "91ict.h"
 
 #define UI_FILE "Dict.glade"
 
 int
 main(int argc, char** argv)
 {
-  ChData     *data;
-  GtkBuilder *builder;
-  GError     *error = NULL;
+     ChData     *data;
+     GtkBuilder *builder;
+     GError     *error = NULL;
 
-  /* Init GTK+ */
-  gtk_init(&argc, &argv);
+     /* Init btree */
+     BTA *tree;
+     btinit();
 
-  /* Create new GtkBuilder object */
-  builder = gtk_builder_new();
-  if (!gtk_builder_add_from_file(builder, UI_FILE, &error))
-  {
-    g_warning("%s", error->message);
-    g_free(error);
-    return 1;
-  }
+     if(check_file_exist("dict_db"))
+          tree = btopn("dict_db", 0, FALSE);
+     else{
+          tree = btcrt("dict_db", 0, FALSE);
+          foldoc_load_2_tree(tree, "FOLDOC");
+     }
 
-  /* Allocate data structure */
-  data = g_slice_new(ChData);
+     /* Init GTK+ */
+     gtk_init(&argc, &argv);
 
-  /* Get objects from UI */
-  #define GW(name) CH_GET_WIDGET(builder, name, data)
-    GW(main_window);
-    GW(dlg_Add);
-    GW(dlg_About);
-    GW(dlg_Delete);
+     /* Create new GtkBuilder object */
+     builder = gtk_builder_new();
+     if (!gtk_builder_add_from_file(builder, UI_FILE, &error))
+     {
+          g_warning("%s", error->message);
+          g_free(error);
+          return 1;
+     }
 
-    /* Get more objects to use */
-  #undef GW
+     /* Allocate data structure */
+     data = g_slice_new(ChData);
 
-  /* Connect signals */
-  gtk_builder_connect_signals(builder, data);
+     /* Get objects from UI */
+#define GW(name) CH_GET_WIDGET(builder, name, data)
+     GW(main_window);
+     GW(dlg_Add);
+     GW(dlg_About);
+     GW(dlg_Delete);
 
-  /* Destroy builder, since we don't need it anymore */
-  g_object_unref(G_OBJECT(builder));
+     GW(word_entry_add_dlg);
+     GW(meaning_txt_add_dlg);
+     GW(tree_completion);
+     GW(search_word);
+     GW(txt_meaning);
+     GW(list_store);
+     data->tree = tree;
+     /* Get more objects to use */
+#undef GW
 
-  /* Show window */
-  gtk_widget_show(data -> main_window);
+     /* Connect signals */
+     gtk_builder_connect_signals(builder, data);
 
-  /* Start main loop */
-  gtk_main();
+     /* Destroy builder, since we don't need it anymore */
+     g_object_unref(G_OBJECT(builder));
 
-  /* Free any allocated data */
-  g_slice_free(ChData, data);
+     /* Show window */
+     gtk_widget_show(data -> main_window);
 
-  return(0);
+     /* Start main loop */
+     gtk_main();
+
+     /* Free any allocated data */
+     g_slice_free(ChData, data);
+
+     return(0);
 }
 
